86d8c132d0ab2794323d051fcf489c3e329dfebb
github username <meingorn@gmail.com>
2017-06-08 17:44:50 -0400

    almost fixed sharing

--- js/controllers/spotagram.js
+++ js/controllers/spotagram.js
@@ -1,16 +1,45 @@
-
-//auth request
-const client_id = client_id
-const response_type = 'token'
-const redirect_uri = 'https://spotagram.herokuapp.com/'
+//
+// //auth request
+// const client_id = client_id
+// const response_type = 'token'
+// const redirect_uri = 'https://spotagram.herokuapp.com/'
 
 //needs auth now, attempting to do it without login which may not be an option
+const url = "https://accounts.spotify.com/api/token";
+const params = { grant_type: "client_credentials" };
+const auth = "Basic " + authentication;
+
+$.ajax(
+  {
+      method: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      data: {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": myurl,
+        "client_secret": mysecret,
+        "client_id": myid,
+      },
+      sucess: function(result) {
+      url: url,
+      type: 'POST',
+      dataType: 'json',
+      headers: {
+          'Authorization' : auth
+      },
+      data: params,
+        success: function(data) {
+        console.log('success', data);
+    }
+  }
+});
+
 function searchTracks(query) {
   $.ajax({url: 'https://api.spotify.com/v1/search',
           headers: {
             'Authorization': 'Bearer ' +
-          }
-          data: {q: query, type: 'track'},
+
+          {data: {q: query, type: 'track'}}},
           success: function(response) {trackWasFound(response)}
   })
 }
